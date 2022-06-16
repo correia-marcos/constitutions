2e31f7eb9038e365e1e5110197eb4f0d53ac0621
correia-marcos <marcospaulorcorreia@gmail.com>
2022-06-16 16:10:39 -0300

    Better labels, better programing and reducing results folder

---
+++ 4_Adj_measures.py
@@ -0,0 +1,392 @@
+# -*- coding: utf-8 -*-
+"""
+In this program, we compute some important controls variables.
+
+Also, we compute adjusted measures of our project:
+
+    Change between first and second constitutions divided by number of years
+    of first regime
+
+
+    Change between first and current constitution, divided by number of years
+    of constitutional government in total
+
+    Change between first and current constitution, with no adjustment for
+    years of regime in total
+
+
+"""
+import numpy as np
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+FOLDER = r'results/csv'
+
+# Reading all csv files (comparing first const with all others)
+lda_prime = pd.read_csv(f'{FOLDER}/comp_prime_50topics(fullset_08_05).csv',
+                        index_col=0)
+tfidf_prime = pd.read_csv(f'{FOLDER}/prime_tfidf.csv',
+                          index_col=0)
+use_prime = pd.read_csv(f'{FOLDER}/prime_use.csv',
+                        index_col=0)
+stm_prime = pd.read_csv(f'{FOLDER}/stm_prime_50topics(fullset_03_06).csv',
+                        index_col=0)
+
+# Reading all csv files (comparing each const with their sucessor)
+lda_seq = pd.read_csv(f'{FOLDER}/comp_seq_50topics(fullset_08_05).csv',
+                      index_col=0)
+tfidf_seq = pd.read_csv(f'{FOLDER}/sequence_tfidf.csv',
+                        index_col=0)
+use_seq = pd.read_csv(f'{FOLDER}/sequence_use.csv',
+                      index_col=0)
+stm_seq = pd.read_csv(f'{FOLDER}/stm_seq_50topics(fullset_03_06).csv',
+                      index_col=0)
+
+# =============================================================================
+# =============================================================================
+# FOR NOW ON, LET'S FIRST ANALYZE THE PRIME DATAFRAMES
+# AFTER WE ADJUST FOR SEQUENCE DAtAFRAMES
+# =============================================================================
+# =============================================================================
+
+# =============================================================================
+# Important remark: tfidf and USE semantic similarity were measured based on
+# cosine similarity. In order to define the distance between constitutions,
+# we use cosine distance which is: distance =  1 - cosine similarity.
+# For more information, see:
+# https://en.wikipedia.org/wiki/Cosine_similarity
+# docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cdist.html
+# =============================================================================
+
+# Changing columns names and creating cosine distance
+tfidf_prime['similarity'] = 1 - tfidf_prime['similarity']
+
+tfidf_prime['similarity'] = np.where(tfidf_prime['similarity'] <= 8e-06,
+                                     0,
+                                     tfidf_prime['similarity'])
+
+use_prime['similarity'] = 1 - use_prime['similarity']
+
+use_prime['similarity'] = np.where(use_prime['similarity'] <= 4e-06,
+                                   0,
+                                   use_prime['similarity'])
+
+tfidf_prime.columns = ['country', 'year', 'distance_tfidf']
+lda_prime.columns = ['country', 'year', 'distance_lda50']
+use_prime.columns = ['country', 'year', 'distance_use']
+stm_prime.columns = ['country', 'year', 'distance_stm50']
+
+# Just to make sure that all dataframes are sorted
+tfidf_prime = tfidf_prime.sort_values(by=['country', 'year'])
+tfidf_prime = tfidf_prime.reset_index(drop=True)
+
+lda_prime = lda_prime.sort_values(by=['country', 'year'])
+lda_prime = lda_prime.reset_index(drop=True)
+
+use_prime = use_prime.sort_values(by=['country', 'year'])
+use_prime = use_prime.reset_index(drop=True)
+
+stm_prime = stm_prime.sort_values(by=['country', 'year'])
+stm_prime = stm_prime.reset_index(drop=True)
+
+# Creating unique dataframe for all measures and the necessary data structure
+
+# IMPORTANT: DON'T CHANGE THIS ORDER UNLESS Y CHANGE THE ORDER ON THE FUNCTION
+df_all = tfidf_prime.copy()
+df_all = df_all.merge(lda_prime, how='outer')
+df_all = df_all.merge(use_prime, how='left')
+df_all = df_all.merge(stm_prime, how='outer')
+
+# Save correlations as df
+corr = df_all.corr()
+corr.to_csv(f'{FOLDER}/correlations(fullset_03_06).csv')
+
+
+def get_adjusted(dataset):
+    """
+    Create adjusted measures and new control variables.
+
+    First, we collect the year of the first constitution in each country.
+    Second, we collect the distance between first and second constitutions.
+    Third, we collect the distance between first and current constitutions.
+    Fourth, we calculate the distance between first and second constitutions
+    divided by number of years of first regime.
+    Fifth, we calculate the distance between first and current constitution,
+    divided by number of years of constitutional government in total.
+    Sixth, we calculate distance between first and current constitution,
+    with no adjustment for years of regime in total.
+    Finally, we construct a Dataframe with all this variables in 204 countries.
+
+    Parameters
+    ----------
+    dataset : dataframes
+        we groupby the dataframe of all constitutions distances based on
+        countries.
+
+    Returns
+    -------
+    Dataframe containing all 192 countries and all necessary variables.
+
+    """
+    # Data structure we need
+    base = dataset.set_index('country')
+    df_group = base.groupby(['country'])
+    dfs_dict = dict(tuple(df_group))
+
+    # Defining necessary variables
+    countries = []
+    first_constitution_year = []
+    second_constitution_year = []
+    change_first_second_tfidf = []
+    change_first_current_tfidf = []
+    change_first_second_lda = []
+    change_first_current_lda = []
+    change_first_second_use = []
+    change_first_current_use = []
+    change_first_second_stm = []
+    change_first_current_stm = []
+
+    # loop to get all necessary data. Note that many countries have only
+    # one constitution so we made a simple if condition on this.
+
+    for country in dfs_dict:
+        countries.append(country)
+        if dfs_dict[country].shape[0] > 1:
+            year = dfs_dict[country].iloc[0, 0]
+            year_second = dfs_dict[country].iloc[1, 0]
+            change_small_tfidf = dfs_dict[country].iloc[1, 1]
+            change_big_tfidf = dfs_dict[country].iloc[-1, 1]
+            change_small_lda = dfs_dict[country].iloc[1, 2]
+            change_big_lda = dfs_dict[country].iloc[-1, 2]
+            change_small_use = dfs_dict[country].iloc[1, 3]
+            change_big_use = dfs_dict[country].iloc[-1, 3]
+            change_small_stm = dfs_dict[country].iloc[1, 4]
+            change_big_stm = dfs_dict[country].iloc[-1, 4]
+
+            first_constitution_year.append(year)
+            second_constitution_year.append(year_second)
+
+            change_first_second_tfidf.append(change_small_tfidf)
+            change_first_second_lda.append(change_small_lda)
+            change_first_second_use.append(change_small_use)
+            change_first_second_stm.append(change_small_stm)
+
+            change_first_current_tfidf.append(change_big_tfidf)
+            change_first_current_lda.append(change_big_lda)
+            change_first_current_use.append(change_big_use)
+            change_first_current_stm.append(change_big_stm)
+
+        else:
+            year = dfs_dict[country].iloc[0, 0]
+            year_second = np.nan
+            change_small_tfidf = np.nan
+            change_big_tfidf = np.nan
+            change_small_lda = np.nan
+            change_big_lda = np.nan
+            change_small_use = np.nan
+            change_big_use = np.nan
+            change_small_stm = np.nan
+            change_big_stm = np.nan
+
+            first_constitution_year.append(year)
+            second_constitution_year.append(year_second)
+
+            change_first_second_tfidf.append(change_small_tfidf)
+            change_first_second_lda.append(change_small_lda)
+            change_first_second_use.append(change_small_use)
+            change_first_second_stm.append(change_small_stm)
+
+            change_first_current_tfidf.append(change_big_tfidf)
+            change_first_current_lda.append(change_big_lda)
+            change_first_current_use.append(change_big_use)
+            change_first_current_stm.append(change_big_stm)
+
+    df = pd.DataFrame({'country': countries,
+                       '1st_const_year': first_constitution_year,
+                       '2nd_const_year': second_constitution_year,
+                       '1st_2nd_tfidf': change_first_second_tfidf,
+                       '1st_current_tfidf': change_first_current_tfidf,
+                       '1st_2nd_lda': change_first_second_lda,
+                       '1st_current_lda': change_first_current_lda,
+                       '1st_2nd_use': change_first_second_use,
+                       '1st_current_use': change_first_current_use,
+                       '1st_2nd_stm': change_first_second_stm,
+                       '1st_current_stm': change_first_current_stm})
+
+    return df
+
+
+data = get_adjusted(df_all)
+
+# Creating one more variables giving the one we create
+data['constitutional_time'] = 2022 - data['1st_const_year']
+data['first_regime_time'] = data['2nd_const_year'] - data['1st_const_year']
+
+# Many countries didn't change their constitutions. We add the first regime
+# time as the constitutional time for this situation
+
+data.first_regime_time.fillna(data.constitutional_time, inplace=True)
+
+# Last definition of variables. There is what we were thinking of as possible
+# adjusted measures
+
+
+def get_adj_measures(data):
+    """Create adjusted measures that we thought as good ones."""
+    data['1st_2nd_tfidf_adj'] = data['1st_2nd_tfidf']/data['first_regime_time']
+    data['1st_2nd_lda_adj'] = data['1st_2nd_lda']/data['first_regime_time']
+    data['1st_2nd_use_adj'] = data['1st_2nd_use']/data['first_regime_time']
+    data['1st_2nd_stm_adj'] = data['1st_2nd_stm']/data['first_regime_time']
+
+    data['1st_curr_tfidf_adj'] = data['1st_current_tfidf'] / \
+        data['constitutional_time']
+    data['1st_curr_lda_adj'] = data['1st_current_lda'] / \
+        data['constitutional_time']
+    data['1st_curr_use_adj'] = data['1st_current_use'] / \
+        data['constitutional_time']
+    data['1st_curr_stm_adj'] = data['1st_current_stm'] / \
+        data['constitutional_time']
+
+
+get_adj_measures(data)
+
+# Open csv with countries codes and merge to data
+
+codes = pd.read_csv(r'Data/country_codes/codes.csv', sep=';')
+
+data = pd.merge(left=data, right=codes,
+                left_on='country', right_on='country',
+                how='left', copy=False)
+data = data.drop('Unnamed: 0', axis=1)
+
+# Changing cols names and columns positions
+data = data.rename(columns={'country_code': 'code'})
+cols = ['code', 'country', '1st_const_year', '2nd_const_year', '1st_2nd_tfidf',
+        '1st_current_tfidf', '1st_2nd_lda', '1st_current_lda', '1st_2nd_use',
+        '1st_current_use', '1st_2nd_stm', '1st_current_stm',
+        'constitutional_time', 'first_regime_time', '1st_2nd_tfidf_adj',
+        '1st_2nd_lda_adj', '1st_2nd_use_adj', '1st_2nd_stm_adj',
+        '1st_curr_tfidf_adj', '1st_curr_lda_adj', '1st_curr_use_adj',
+        '1st_curr_stm_adj']
+data = data[cols]
+
+# Save data
+data.to_csv(f'{FOLDER}/textual_variables_prime(03_06).csv')
+
+# Save dataframe with all correlations
+matrix = data.corr()
+matrix.to_csv(f'{FOLDER}/all_correlations(03_06).csv')
+
+# =============================================================================
+# =============================================================================
+# SEQUENCE DATAFRAME TIME
+# =============================================================================
+# =============================================================================
+
+# Changing columns names and creating cosine distance of the sequence_dataframe
+tfidf_seq['similarity'] = 1 - tfidf_seq['similarity']
+
+tfidf_seq['similarity'] = np.where(tfidf_seq['similarity'] <= 8e-06,
+                                   0,
+                                   tfidf_seq['similarity'])
+
+use_seq['similarity'] = 1 - use_seq['similarity']
+
+use_seq['similarity'] = np.where(use_seq['similarity'] <= 4e-06,
+                                 0,
+                                 use_seq['similarity'])
+
+tfidf_seq.columns = ['country', 'year', 'distance_tfidf']
+lda_seq.columns = ['country', 'year', 'distance_lda50']
+use_seq.columns = ['country', 'year', 'distance_use']
+stm_seq.columns = ['country', 'year', 'distance_stm50']
+
+# Just to make sure that all dataframes are sorted
+tfidf_seq = tfidf_seq.sort_values(by=['country', 'year'])
+tfidf_seq = tfidf_seq.reset_index(drop=True)
+
+lda_seq = lda_seq.sort_values(by=['country', 'year'])
+lda_seq = lda_seq.reset_index(drop=True)
+
+use_seq = use_seq.sort_values(by=['country', 'year'])
+use_seq = use_seq.reset_index(drop=True)
+
+stm_seq = stm_seq.sort_values(by=['country', 'year'])
+stm_seq = stm_seq.reset_index(drop=True)
+
+# Creating unique dataframe for all measures and the necessary data structure
+
+# IMPORTANT: DON'T CHANGE THIS ORDER UNLESS YOU CHANGE THE ORDER ON THE
+# FUNCTION
+df_all_seq = tfidf_seq.copy()
+df_all_seq = df_all_seq.merge(lda_seq, how='outer')
+df_all_seq = df_all_seq.merge(use_seq, how='left')
+df_all_seq = df_all_seq.merge(stm_seq, how='outer')
+
+# corr = df_all_seq.corr()
+
+
+def get_adjusted_seq(dataset):
+    """
+    Create adjusted measures and new control variables.
+
+    First, we collect the year of the first constitution in each country.
+    Second, we collect the distance between first and second constitutions.
+    Finally, we construct a Dataframe with all this variables in 204 countries.
+
+    Parameters
+    ----------
+    dataset : DataFrame
+        we groupby the dataframe of all constitutions distances based on
+        countries.
+
+    Returns
+    -------
+    Dataframe containing all 192 countries and all necessary variables.
+
+    """
+    # Defining necessary variables
+    base_seq = dataset.set_index('country')
+    df_group_seq = base_seq.groupby(['country'])
+    dfs_seq_dict = dict(tuple(df_group_seq))
+
+    countries = []
+    tfidf_change = []
+    lda_change = []
+    use_change = []
+    stm_change = []
+
+    # Loop to get all necessary values in the dict formed of
+    # dataframes
+    for country in dfs_seq_dict:
+        countries.append(country)
+        all_change_tfidf = dfs_seq_dict[country].iloc[:, 1].sum()
+        all_change_lda = dfs_seq_dict[country].iloc[:, 2].sum()
+        all_change_use = dfs_seq_dict[country].iloc[:, 3].sum()
+        all_change_stm = dfs_seq_dict[country].iloc[:, 4].sum()
+
+        tfidf_change.append(all_change_tfidf)
+        lda_change.append(all_change_lda)
+        use_change.append(all_change_use)
+        stm_change.append(all_change_stm)
+
+    df_change = pd.DataFrame({'country': countries,
+                              'tfidf_distance': tfidf_change,
+                              'lda_distance': lda_change,
+                              'use_distance': use_change,
+                              'stm_distance': stm_change})
+
+    return df_change
+
+
+# Get and save dataset
+data_seq = get_adjusted_seq(df_all_seq)
+
+final_data = pd.merge(left=data, right=data_seq,
+                      left_on='country', right_on='country',
+                      how='left', copy=False)
+# Saving dataset
+final_data.to_csv(f'{FOLDER}/all_textual_variables(03_06).csv')
+profile = ProfileReport(final_data,
+                        title="Pandas Profiling Report")
+profile.to_file(r'results/HTML/textual_data.html')
---
+++ 5_Agg_cov.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+"""
+Add a lot of covariates.
+
+We took data from a lot o sources, from Word Bank and Polity 4
+to some papers (see https://scholar.harvard.edu/shleifer/publications)
+
+@author: marcos
+"""
+import pandas as pd
+import numpy as np
+from pandas_profiling import ProfileReport
+
+FOLDER = r'results/csv'
+
+data = pd.read_csv(fr'{FOLDER}/all_textual_variables(03_06).csv',
+                   index_col=False)
+data = data.drop('Unnamed: 0', axis=1)
+
+
+# The way we exported data from Word_bank, the dataset were in a
+# "tidy data" format that I don't use much and is not good for regressions.
+# The function below was done to create a dataset
+# in the right way for running regressions.
+
+
+def get_table(dataset_text, covariates_data, index='Series Name'):
+    """Get the merged dataframe with dataset_text and covariates or others.
+
+    Args:
+        dataset_text (DataFrame): Dataframe created before with textual_data
+        covariates_data (DataFrame): all Dataframes from World Bank works
+
+    Returns:
+        DataFrame: merge dataframe
+    """
+    covariates_data = covariates_data.set_index(index)
+    df_grouped = covariates_data.groupby([index])
+    dfs = dict(tuple(df_grouped))
+
+    dataframe = dataset_text.copy()
+    for variable in dfs:
+        columns = list(dfs[variable].columns)
+        for i in range(len(columns)-1):
+            columns[i+1] = variable + ' - ' + str(columns[i+1])
+        dfs[variable].columns = columns
+        dataframe = dataframe.merge(dfs[variable], left_on='code',
+                                    right_on='codes', how='left',
+                                    suffixes=('', '_delme'))
+    # We have a lot of columns with same labels and values, so we removed then
+    dataframe = dataframe[[c for c in dataframe.columns if not
+                           c.endswith('_delme')]]
+
+    return dataframe
+
+# =============================================================================
+# COVARIATES FROM DIFFERENT DATASETS
+# =============================================================================
+
+# AHDI FROM LAPORTA
+# Adding info from the AHDI file in Data/AHDI
+
+ahdi = pd.read_excel('Data/AHDI/AHDI_1.1-1.xlsx', sheet_name='AHDI')
+ahdi.columns = ['code', 'country_ahdi', 'ahdi_1870', 'ahdi_1880',
+                'ahdi_1890',
+                'ahdi_1900', 'ahdi_1913', 'ahdi_1925', 'ahdi_1929',
+                'ahdi_1933', 'ahdi_1938', 'ahdi_1950', 'ahdi_1955',
+                'ahdi_1960', 'ahdi_1965', 'ahdi_1970', 'ahdi_1975',
+                'ahdi_1980', 'ahdi_1985', 'ahdi_1990', 'ahdi_1995',
+                'ahdi_2000', 'ahdi_2005', 'ahdi_2007', 'ahdi_2010',
+                'ahdi_2015']
+# =============================================================================
+# SOME COVARIATES FROM WORD BANK
+# =============================================================================
+
+# Adding others excel files with control and dependent variable for
+# future regressions
+# Covariates_1 is the bigger file and covariates_2 add some variables like
+# total debt service (% of GNI)
+covariates1 = pd.read_excel(r'Data/Word_bank/covariates_1.xlsx',
+                            sheet_name='Data')
+# We must remove unnecessary columns
+covariates1 = covariates1.drop('Series Code', axis=1)
+covariates1 = covariates1.drop('Country Name', axis=1)
+
+data_cov = get_table(data, covariates1)
+
+# ==============================================================================
+# Covariates_2 data from Word Bank
+# ==============================================================================
+covariates2 = pd.read_excel(r'Data/Word_bank/covariates_2.xlsx',
+                            sheet_name='Data')
+
+data_cov = get_table(data_cov, covariates2)
+
+# =============================================================================
+# GOVERNANCE INDICATORS FROM WORLD BANK
+# =============================================================================
+
+governance = pd.read_excel(r'Data/Word_bank/Governance_indicators.xlsx',
+                           sheet_name='Data')
+
+data_cov = get_table(data_cov, governance)
+
+# WORLD BANK REAL INTEREST
+# adding real interest
+# =============================================================================
+real_interest = pd.read_excel(r'Data/Word_bank/Real_interest_rate.xlsx',
+                              sheet_name='Data')
+
+data_cov = get_table(data_cov, real_interest, index='Indicator Name')
+
+# We noticed some columns were not read as number. We noticed that empty values
+# were coded as ".." which make pythons misunderstand the variable type
+data_cov = data_cov.replace("..", np.nan)
+
+# =============================================================================
+# CPP PROJECT COVARIATES (SIZE OF CONSTITUTIONS, AMOUNT OF AMENDMENTS)
+# =============================================================================
+cpp_data = pd.read_csv(fr'{FOLDER}/constitutions_for_stm.csv')
+cpp_data = cpp_data.drop('document', axis=1)
+cpp_data = cpp_data.drop('Unnamed: 0', axis=1)
+cpp_data.columns = ['country', 'codes', 'year', 'length_preprocess_lastconst',
+                    'length_lastconst',
+                    'col.belgium', 'col.britain', 'col.france', 'col.germany',
+                    'col.italy', 'col.netherlands', 'col.portugal',
+                    'col.spain']
+
+cpp_data = cpp_data.drop_duplicates(['country'], keep='last')
+cpp_data = cpp_data.drop('year', axis=1)
+
+
+# Merging with the dataframe
+df_added = pd.merge(data_cov, cpp_data, how='left', on='codes',
+                    copy=False)
+
+# Dropping unnecessary columns(like percentile estimates, number of sources
+# and estimates of standard errors)
+df_added = df_added[df_added.columns.drop(list(df_added.filter(
+    regex='Percentile')))]
+df_added = df_added[df_added.columns.drop(list(df_added.filter(
+    regex='Source')))]
+df_added = df_added[df_added.columns.drop(list(df_added.filter(
+    regex='Standard Error')))]
+
+# =============================================================================
+# DATA FROM The Economic Consequences of Legal Origins (2008)
+# =============================================================================
+legal_origins = pd.read_excel(r'Data/legal_origins_paper/Data.xlsx',
+                              sheet_name='Table 1')
+
+# Removing unnecessary columns - we only need country code and legal origin
+legal_origins = legal_origins[['code', 'legor_uk', 'legor_fr',
+                               'legor_ge', 'legor_sc', 'legor_so']]
+
+# Adding together dataframe and ahdi
+final_df = pd.merge(df_added, legal_origins, on='code', how='left', copy=False)
+final_df = pd.merge(df_added, ahdi, how='left', on='code', copy=False)
+
+# Cleaning some more
+final_df = final_df.drop('country_y', axis=1)
+final_df = final_df.drop('codes', axis=1)
+final_df = final_df.rename(columns = {'country_x': 'country'})
+
+# =============================================================================
+# Adding up number of amendments
+# =============================================================================
+amendments = pd.read_csv(f'{FOLDER}/amendments.csv')
+amendments = amendments.drop('Unnamed: 0', axis=1)
+final_df = final_df.merge(amendments, on='country',how='left', copy=False)
+
+# Creating Pandas Profiling - too much variables to handle
+# small_df = final_df.iloc[:, 0:50].join(final_df.iloc[:, -50:])
+report = ProfileReport(final_df, title='Pandas Profile Report')
+
+# Saving results
+final_df.to_csv(f'{FOLDER}/all_variables_reg(03_06).csv')
+report.to_file(r'results/HTML/all_variables.html')
