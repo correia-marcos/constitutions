d38109ad10ab3493e7fdf77893c56593ca10b8fd
Marcos Paulo <marcospaulorcorreia@gmail.com>
2024-01-28 13:58:36 -0300

    Change files name and removing not used libraries/lines of code

--- 2_TF_IDF_with_similarity.py
+++ 2_TF_IDF_with_similarity.py
@@ -5,150 +5,19 @@ Program to run tf-idf + cosine similarity between constitutions.
 @author: marcos
 """
 # Import required libraries
-import glob
-import os
+import csv
 import pandas as pd
-import io
-import re
+
 from collections import Counter
 
 import nltk
 from nltk.corpus import stopwords
 
-import spacy
 import gensim
 from gensim.models import TfidfModel
 from gensim.corpora import Dictionary
 from gensim.parsing.preprocessing import STOPWORDS
 from gensim.utils import simple_preprocess
-
-import logging
-import warnings
-
-# Not important to understand, make it easier to see what is happening in
-# the process
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
-                    level=logging.INFO)
-
-# =============================================================================
-# You can run all this again or use the DataFrame Created on LDA.py
-# =============================================================================
-# Local of texts
-loc_total = r'data\all'
-
-# Getting the list of all texts
-all_texts = glob.glob(os.path.join(loc_total, '*.txt'))
-
-
-def get_df(texts):
-    """
-    Create a Df from a list of texts - their name in local memory.
-
-    It makes a DF of the name, year (from its names) and content of each text.
-
-    Parameters
-    ----------
-    texts : TYPE list
-        DESCRIPTION. list of all txt files (constitutions) of the same
-        language.
-
-
-    Returns
-    -------
-    None.
-
-    """
-    countries = []
-    year = []
-    constitution = []
-
-    for txt in texts:
-        country_year = os.path.splitext((os.path.basename(txt)))[0]
-        countries.append(' '.join(country_year.split('_')[:-1]))
-        year.append(country_year.split('_')[-1])
-
-        with io.open(txt, 'r', encoding='utf-8', errors='ignore') as fout:
-            constitution.append(''.join(fout.readlines()))
-
-    corpus = pd.DataFrame({'country': countries, 'year': year,
-                           'constitution': constitution})
-
-    return corpus
-
-
-def lemmatization(texts):
-    """See https://spacy.io/api/annotation."""
-    texts_out = []
-    nlp = spacy.load('en_core_web_lg', disable=['parser', 'ner'])
-    for sent in texts:
-        doc = nlp(sent)
-        texts_out.append([token.lemma_ for token in doc if token.pos_])
-    return texts_out
-
-
-# Creating DF and then applying lemmatization. Important to do before
-# preprocessing.
-corpus_all = get_df(all_texts)
-
-data_all = corpus_all.constitution.tolist()
-all_lemma = lemmatization(data_all)
-all_lemma = [' '.join(item) for item in all_lemma]
-
-
-def clean_text(text):
-    """Clean up text data.
-
-    Make text lowercase, remove text in square brackets, remove
-    punctuation, remove digits in general, remove urls, remove
-    emails and remove "" characters.
-    Also remove some common and bad words such as "Chapter".
-    """
-    text = text.lower()
-    text = re.sub(r'\[.*?\]', '', text)
-    text = re.sub(r'[!,:\-;\.\?\(\)]', '', text)
-    text = ''.join(i for i in text if not i.isdigit())
-    text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text)
-    text = re.sub(r'http\S+', '', text)
-    text = re.sub(r'[‘’“”…]', '', text)
-    text = re.sub('\n', ' ', text)
-    text = re.sub(r'<list>', ' ', text)
-    text = re.sub(r'<title>', ' ', text)
-    text = re.sub(r'<title', ' ', text)
-    text = re.sub(r'<preamble>', ' ', text)
-    text = re.sub(r'</list>', '', text)
-    text = re.sub(r'/', '', text)
-    text = re.sub(r'Chapter', '', text)
-    text = re.sub(r'chapter', '', text)
-    text = re.sub('(\\b[A-Za-z] \\b|\\b [A-Za-z]\\b)', '', text)
-    text = re.sub(r'section', '', text)
-    text = re.sub(r'&', '', text)
-    text = re.sub(r'-(?!\w)|(?<!\w)-', '', text)
-    text = re.sub(r'_', '', text)
-    text = re.sub(r'copyright', '', text)
-    text = re.sub(r'@', '', text)
-    text = re.sub(r'iii', '', text)
-    text = re.sub(r'ii', '', text)
-    text = re.sub(r'vi', '', text)
-    text = re.sub(r'vii', '', text)
-    text = re.sub(r'vii', '', text)
-    text = re.sub(r'viii', '', text)
-    text = re.sub(r'ix', '', text)
-    text = re.sub(r'x', '', text)
-    text = re.sub(r'xi', '', text)
-    text = re.sub(r'xii', '', text)
-    text = re.sub(r'<list>', '', text)
-
-    return text
-
-
-# Adding the new lemmatized data to the DF
-corpus_all['constitution_lemma'] = all_lemma
-# Applying some cleaning
-corpus_all.constitution_lemma = corpus_all['constitution_lemma'].apply(
-    lambda x: clean_text(x))
-
-df = corpus_all
 # =============================================================================
 # Using the Dataframe we already created:
 # =============================================================================
@@ -162,7 +31,7 @@ df = df.reset_index(drop=True)
 def sent_to_words(sentences):
     """Do a simple tokenization."""
     for sentence in sentences:
-        yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))
+        yield gensim.utils.simple_preprocess(str(sentence), deacc=True)
 
 
 # Time to tokening words and create necessary objects
@@ -221,6 +90,13 @@ n_words = id2word.num_pos
 words_list = [word for words in data_words for word in words]
 most_common_words = Counter(words_list).most_common(50)
 
+# Save the list of most frequent words into a csv file
+with open('results/csv/common_words.csv', 'w') as fout:
+
+    # using csv.writer method from CSV package
+    write = csv.writer(fout)
+    write.writerow(most_common_words)
+
 # Create the TF-IDF model
 model = TfidfModel(corpus)
 corpus_tfidf = model[corpus]
@@ -233,12 +109,12 @@ index = gensim.similarities.SparseMatrixSimilarity(model[corpus],
 sims = index[corpus_tfidf]
 sims_df = pd.DataFrame(sims)
 
-# The DF that must be saved
+# Creating the DF that must be saved
 df_final = df[['country', 'year']]
 df_final = df_final.join(sims_df)
 
+# Change the columns names
 column_names = (df_final['country'] + '_' + df_final['year']).to_list()
-
 df_final.columns = ['country', 'year'] + column_names
 
 df_final.to_excel('results/tf_idf/constitutions.xlsx')
@@ -262,7 +138,7 @@ for country in dfs:
         similarity_prime.append(value)
 
 final_prime = df[['country', 'year']]
-final_prime['similarity'] = similarity_prime
+final_prime.insert(2, 'simirality', similarity_prime, True)
 
 
 # This small loop do the calculation in the right rows (for comparative in
@@ -277,12 +153,8 @@ for country in dfs:
         similarity_sequence.append(series_next.loc[name_column, ])
 
 final_sequence = df[['country', 'year']]
-final_sequence['similarity'] = similarity_sequence
+final_sequence.insert(2, 'similarity', similarity_sequence, True)
 
 # Finally, we must save data
 final_prime.to_csv('results/csv/prime_tfidf.csv')
-# final_prime.to_excel('results/csv/sequence_tfidf.xlsx')
 final_sequence.to_csv('results/csv/sequence_tfidf.csv')
-# final_sequence.to_excel('results/tf_idf/sequence_tfidf.xlsx')
-final_sequence.to_csv('results/csv/sequence_tfidf.csv')
-# final_sequence.to_excel('results/tf_idf/sequence_tfidf.xlsx')
