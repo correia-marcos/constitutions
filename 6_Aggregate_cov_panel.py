9ab75b687aec48c06910f2f9cd75a5fd14908426
Marcos Paulo <marcospaulorcorreia@gmail.com>
2024-01-28 20:39:33 -0300

    Changes do to new calculation of TFIDF (in the library) and new script added

---
+++ 6_Aggregate_cov_panel.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Add a lot of covariates.
+
+We took data from a lot o sources, from Word Bank and Polity 5
+to some papers (see https://scholar.harvard.edu/shleifer/publications)
+
+@author: marcos
+"""
+
+# Import required libraries
+import pandas as pd
+import numpy as np
